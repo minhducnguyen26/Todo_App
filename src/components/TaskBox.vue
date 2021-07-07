c1bb6514d48bc46554e84477d0ade5fd31a0858d
minhducnguyen26 <minhducnguyen260101@gmail.com>
2021-07-06 23:58:17 -0600

    Styled TaskBox components

--- src/components/TaskBox.vue
+++ src/components/TaskBox.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <div class="task_box_wrapper">
+      <div class="check_box">
+          <i class="check_mark las la-check"></i>
+      </div>
+      
+      <div class="task_name"><slot></slot></div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +14,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .task_box_wrapper {
+        border-radius: 25px;
+        padding: 20px;
+        display: grid;
+        grid-template-columns: 1fr 7fr;
+        background-color: white;
+        margin-bottom: 10px;
+    }
+    .check_box {
+        border: 2px solid var(--lt-purple-neon);
+        border-radius: 50%;
+        text-align: center;
+        padding: 3px 0;
+        margin-right: 16px;
+        font-size: 10px;
+    }
+    .check_mark {
+        color: white;
+    }
+    .task_name {
+        display: flex;
+        align-items: center;
+        font-size: 17px;
+    }
 </style>
